29cc80213e33edd3ab896e3fbd908af611f5ff1f
glug.oleg <glug.oleg@gmail.com>
2023-09-18 01:47:57 +0300

    fix config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/calendar_tt',
+  base: '/calendar_tt/',
   plugins: [react(), svgr()],
 });
